2ec20943140c5aca0a7d7ab34b6ad5dacbf9ff92
isleep59 <jessewang59@gmail.com>
2025-03-23 21:46:22 +0800

    FIX: change the path to data and disable ann_file.

--- classification/configs/_base_/datasets/imagenet_bs64_swin_224.py
+++ classification/configs/_base_/datasets/imagenet_bs64_swin_224.py
@@ -54,18 +54,18 @@ data = dict(
     workers_per_gpu=8,
     train=dict(
         type=dataset_type,
-        data_prefix='data/imagenet/train',
+        data_prefix='/data/imagenet/train',
         pipeline=train_pipeline),
     val=dict(
         type=dataset_type,
-        data_prefix='data/imagenet/val',
-        ann_file='data/imagenet/meta/val.txt',
+        data_prefix='/data/imagenet/val',
+        # ann_file='data/imagenet/meta/val.txt',
         pipeline=test_pipeline),
     test=dict(
         # replace `data/val` with `data/test` for standard test
         type=dataset_type,
-        data_prefix='data/imagenet/val',
-        ann_file='data/imagenet/meta/val.txt',
+        data_prefix='/data/imagenet/val',
+        # ann_file='data/imagenet/meta/val.txt',
         pipeline=test_pipeline))
 
 evaluation = dict(interval=10, metric='accuracy')

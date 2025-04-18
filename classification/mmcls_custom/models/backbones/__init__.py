4712be1730959ec26bee84141fd06457e097ee44
isleep-59 <jessewang59@gmail.com>
2025-04-19 00:50:59 +0800

    FIX: register for OrderFinetuneVRWKV.

--- classification/mmcls_custom/models/backbones/__init__.py
+++ classification/mmcls_custom/models/backbones/__init__.py
@@ -1,4 +1,5 @@
 from .vrwkv import VRWKV
 from .vrwkv6 import VRWKV6
+from .orderfinetune_vrwkv import OrderFinetuneVRWKV
 
-__all__ = ['VRWKV', 'VRWKV6']
+__all__ = ['VRWKV', 'VRWKV6', 'OrderFinetuneVRWKV']

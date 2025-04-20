96ed45b0bcbbc28e040beeb6315cb52c810d5717
isleep-59 <jessewang59@gmail.com>
2025-04-21 01:17:04 +0800

    FIX: delete remaining __init__ content.

--- classification/mmcls_custom/models/backbones/__init__.py
+++ classification/mmcls_custom/models/backbones/__init__.py
@@ -1,5 +1,4 @@
 from .vrwkv import VRWKV
 from .vrwkv6 import VRWKV6
-from .orderfinetune_vrwkv import OrderFinetuneVRWKV
 
-__all__ = ['VRWKV', 'VRWKV6', 'OrderFinetuneVRWKV']
+__all__ = ['VRWKV', 'VRWKV6']
